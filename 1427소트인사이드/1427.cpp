16bfc4db34b5139c9083a9326e4a2590b793b05f
kindsmell <kindsmell33@gmail.com>
2023-10-12 19:41:06 +0900

    substr로 하면 안 됐던 이유

--- 1427소트인사이드/1427.cpp
+++ 1427소트인사이드/1427.cpp
@@ -14,7 +14,9 @@ int main()
     vector<int> num(str.size(), 0);
 
     for(int i = 0; i < str.size(); i++)
-        num[i] = (str[i] - '0'); //str도 인덱스로 접근이
+        num[i] = (str[i] - '0'); // str도 인덱스로 접근 가능
+        //Using str.substr(i, 1) - '0' is not the correct way to convert a character to its integer value because str.
+        //substr(i, 1) returns a substring of str as a new string, not a single character. The subtraction operator (-) is not defined for strings, so you would get a compilation error.
 
     for(int i = 0; i < str.size(); i++)
     {
