adee6efe9a9e640a88bba41407202a25df1d42f6
kindsmell <kindsmell33@gmail.com>
2023-10-22 17:04:09 +0900

    등차수열 별찍기

---
+++ 2444별찍기-7/2444.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < n - i ; j++)
+            cout << " ";
+        for(int k = 0; k < 2 * i - 1; k++)
+            cout << "*";
+        cout << "\n";
+    }
+    
+    for(int i = n - 1; i > 0; i--)
+    {
+        for(int j = 0; j < n - i; j++)
+            cout << " ";
+        for(int k = 0; k < 2 * i - 1; k++)
+            cout << "*";
+        cout << "\n";
+    }
+}
