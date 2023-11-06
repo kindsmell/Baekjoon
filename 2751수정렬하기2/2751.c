94c7ed307f8d81e73209550cecf7dfd20d8ad4b3
kindsmell <kindsmell33@gmail.com>
2023-11-06 13:49:19 +0900

    재귀함수 복습

--- 2751수정렬하기2/2751.c
+++ 2751수정렬하기2/2751.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void merge(int list[], int left, int mid, int right) {
+void merge(int list[], int left, int mid, int right)
+{
 
   int sorted[1000000];
   int i, j, k, l;
@@ -8,7 +9,7 @@ void merge(int list[], int left, int mid, int right) {
   j = mid + 1;
   k = left;
 
-  while (i <= mid && j <= right) //둘 중 하나라도 끝에 도달하면 끝
+  while (i <= mid && j <= right) // 둘 중 하나라도 끝에 도달하면 끝
   {
     if (list[i] <= list[j])
       sorted[k++] = list[i++];
@@ -20,42 +21,48 @@ void merge(int list[], int left, int mid, int right) {
   {
     for (l = j; l <= right; l++)
       sorted[k++] = list[l];
-  } else // mid+1에서 시작한 그룹이 끝에 도달
+  }
+  else // mid+1에서 시작한 그룹이 끝에 도달
   {
     for (l = i; l <= mid; l++)
       sorted[k++] = list[l];
-  } //여기까지 하면 sorted에 정렬되어서 담겨짐
+  } // 여기까지 하면 sorted에 정렬되어서 담겨짐
 
-  //옮기기
+  // 옮기기
   for (l = left; l <= right; l++)
     list[l] = sorted[l];
 }
 //
-void merge_sort(int list[], int left, int right) {
+void merge_sort(int list[], int left, int right)
+{
   int mid;
 
-  //재귀함수
-  if (left < right) {
+  // 재귀함수
+  if (left < right)
+  { // left랑 right랑 같거나 rigth가 더 작으면 종료됨. 한칸단위까지 자른다는 의미
     mid = (left + right) / 2;
-    merge_sort(list, left, mid);
-    merge_sort(list, mid + 1, right);
-    merge(list, left, mid, right);
+    merge_sort(list, left, mid);      // 앞쪽부분 정렬
+    merge_sort(list, mid + 1, right); // 뒤쪽부분 정렬
+    merge(list, left, mid, right);    // 정렬된 두개 합병
   }
 }
 
-int main() {
+int main()
+{
   int list[1000000];
   int n;
 
   scanf("%d", &n);
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     scanf("%d", &list[i]);
   }
 
   merge_sort(list, 0, n - 1);
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     printf("%d\n", list[i]);
   }
 }
