589bc2c69b925007c60fcd9c6496a56c177fa1b8
kindsmell <kindsmell33@gmail.com>
2023-11-09 14:17:15 +0900

    make_pair정리

--- 1755숫자놀이/1755.cpp
+++ 1755숫자놀이/1755.cpp
@@ -16,7 +16,9 @@ int main()
         if (i >= 10)
         {
             A = eng[i / 10] + " " + eng[i % 10];
-            rnt.push_back({A, i}); // make pair vs {}
+            rnt.push_back({A, i}); // make_pair보다 가독성이 좋음
+            // rnt.push_back(make_pair(A, i)); // 인자들의 타입을 자동매칭
+            //  rnt.push_back(pair<string, int>(A,i)); //인자의 타입을 명시해줘야함
         }
         else
         {
@@ -25,7 +27,7 @@ int main()
         }
     }
 
-    sort(rnt.begin(), rnt.end());
+    sort(rnt.begin(), rnt.end()); // pair를 sort하게 되면, 첫번째 인자를 기준으로 정렬
 
     for (int i = 0; i < rnt.size(); i++)
     {
